d6726cf07d0bfe123623bdf94faf909584716150
Ken Reed <github@kmreed.net>
2018-11-14 11:18:20 -0800

    Progress
    - Added dummy data which gets displayed on the home page.
    - Updates to the Wiki to record progess.
    - Changed about and home to inherit from layout.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ken Reed',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '2018-11-14 10:06'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '2018-11-14 10:07'
+    },
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
